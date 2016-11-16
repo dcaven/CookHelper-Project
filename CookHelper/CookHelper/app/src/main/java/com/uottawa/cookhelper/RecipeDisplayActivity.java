bd4e1e1fb23ed6a1e053a9628f94ca214291c2a4
David Caven <d_caven4@hotmail.com>
2016-11-16 15:34:49 -0500

    Main Project Files
    Uploaded files from non functional UI assignment

---
+++ CookHelper/CookHelper/app/src/main/java/com/uottawa/cookhelper/RecipeDisplayActivity.java
@@ -0,0 +1,13 @@
+package com.uottawa.cookhelper;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RecipeDisplayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe_display);
+    }
+}
