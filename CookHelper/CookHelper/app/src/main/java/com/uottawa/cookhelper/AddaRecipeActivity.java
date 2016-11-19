1f44cb0857b91bdfabadd019fbd9453e735ea1b0
Leisha Burford <leishabobisha@gmail.com>
2016-11-19 17:36:52 -0500

    fixed error, background colour not updating

--- CookHelper/CookHelper/app/src/main/java/com/uottawa/cookhelper/AddaRecipeActivity.java
+++ CookHelper/CookHelper/app/src/main/java/com/uottawa/cookhelper/AddaRecipeActivity.java
@@ -2,6 +2,7 @@ package com.uottawa.cookhelper;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class AddaRecipeActivity extends AppCompatActivity {
 
@@ -10,4 +11,8 @@ public class AddaRecipeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adda_recipe);
     }
+
+
 }
+
+
--- CookHelper/CookHelper/app/src/main/java/com/uottawa/cookhelper/HomeActivity.java
+++ CookHelper/CookHelper/app/src/main/java/com/uottawa/cookhelper/HomeActivity.java
@@ -1,7 +1,10 @@
 package com.uottawa.cookhelper;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +12,32 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+
+
+        Button btnMyRecipe= (Button) findViewById(R.id.btnMyRecipe);
+        Button btnAddRecipe= (Button) findViewById(R.id.btnAddRecipe);
+        Button btnFindRecipe= (Button) findViewById(R.id.btnFindRecipe);
+
+        btnMyRecipe.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                startActivity(new Intent(getApplicationContext(),RecipeDisplayActivity.class));
+            }
+        });
+
+        btnAddRecipe.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                startActivity(new Intent(getApplicationContext(),AddaRecipeActivity.class));
+            }
+        });
+
+        btnFindRecipe.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                startActivity(new Intent(getApplicationContext(),SearchRecipeActivity.class));
+            }
+        });}
+
+
     }
-}
+
+
