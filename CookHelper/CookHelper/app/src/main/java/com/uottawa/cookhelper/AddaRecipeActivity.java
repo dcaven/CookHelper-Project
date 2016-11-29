adb9ca540aa589e00cd0e7d4464331fde8773fd9
JoeCodey <joseph-lef@joseph-lefebvres-MacBook-Pro.local>
2016-11-29 01:07:06 -0500

    > Changed AndroidManifest.XML file to enable debugger inorder to text on real android hardware > Included an ActionListener in addaRecipeActivity.java so that after the user enter the title of the receipe and presses "ENTER" , the keyboard disappears so they can see the rest of the activity.

--- CookHelper/CookHelper/app/src/main/java/com/uottawa/cookhelper/AddaRecipeActivity.java
+++ CookHelper/CookHelper/app/src/main/java/com/uottawa/cookhelper/AddaRecipeActivity.java
@@ -2,7 +2,16 @@ package com.uottawa.cookhelper;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.View.OnKeyListener ;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.EditText;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager ;
+import android.content.Context ;
+
 
 public class AddaRecipeActivity extends AppCompatActivity {
 
@@ -10,8 +19,42 @@ public class AddaRecipeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adda_recipe);
+
+        Button btnAdd = (Button) findViewById(R.id.btnMyRecipe);
+        TextView recipeLabel = (TextView) findViewById(R.id.txtAddRecipe); //Finds the recipeLabel i.e. the top TextEdit
+        recipeLabel.setOnEditorActionListener(recipelabelListener);// Creates and action listner for the top label
     }
 
+    /*
+        This ActionListner for the recipeLabel simply hides the keyboard when the user
+        triggers an action event by pressing the "ENTER" or "RETURN" after
+        Entering their Recipe title
+        IMPLEMENTED : NOV 29th : 12:57 AM 
+     */
+    TextView.OnEditorActionListener recipelabelListener = new TextView.OnEditorActionListener(){
+        public boolean onEditorAction(TextView recipeLabel, int actionId, KeyEvent event) {
+            if (actionId == EditorInfo.IME_NULL
+                    && event.getAction() == KeyEvent.ACTION_DOWN) {
+                InputMethodManager imm = (InputMethodManager)getSystemService(Context.
+                        INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+            }
+            return true;
+        }
+    } ;
+
+//    EditText.setOnKeyListener(new OnKeyListener() {
+//            public boolean onKey(View , int keyCode, KeyEvent keyevent) {
+//            //If the keyevent is a key-down event on the "enter" button
+//            if ((keyevent.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
+//                //...
+//                // Perform your action on key press here
+//                // ...
+//                return true;
+//            }
+//            return false;
+//        }
+//    });
 
 }
 
