164f6478c15258b73b6936c896d20e210df87a21
JoeCodey <jlefe074@uottawa.ca>
2016-12-03 12:30:23 -0500

    small changes

--- CookHelper/CookHelper/app/src/main/java/com/uottawa/cookhelper/AddaRecipeActivity.java
+++ CookHelper/CookHelper/app/src/main/java/com/uottawa/cookhelper/AddaRecipeActivity.java
@@ -47,6 +47,7 @@ public class AddaRecipeActivity extends AppCompatActivity {
                 InputMethodManager imm = (InputMethodManager)getSystemService(Context.
                         INPUT_METHOD_SERVICE);
                 imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+
             }
             return true;
         }
